e645d3c91685be94547a5e8cd4bd046a6f229447
ahribori <ahribori@gmail.com>
2019-02-15 00:08:33 +0900

    Text, Indicator, Button example

--- screens/MyScreen.js
+++ screens/MyScreen.js
@@ -1,27 +1,70 @@
 import React from 'react';
-import { ScrollView, View, StyleSheet, Text } from 'react-native';
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  Text,
+  ActivityIndicator,
+  Button,
+} from 'react-native';
+
+const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
 
 export default class MyScreen extends React.Component {
-    render() {
-        return (
-            <ScrollView style={styles.container}>
-                <View style={ styles.subContainer}>
-                    <Text>Hello, world</Text>
-                </View>
-            </ScrollView>
-        )
-    }
+  render() {
+    const navigationView = (
+      <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+      </View>
+    );
+
+    return (
+      <ScrollView style={styles.container}>
+        <Title>Text</Title>
+        <View style={styles.subContainer}>
+          <Text style={styles.text}>Hello, world!</Text>
+        </View>
+
+        <Title>Activity Indicator</Title>
+        <View style={[styles.subContainer, styles.center]}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+
+        <Title>Button</Title>
+        <View style={{ padding: 16 }}>
+          <Button
+            onPress={e => {
+              console.log(e);
+            }}
+            title="Test"
+            color="#0093EF"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+
+        <Title>DrawerLayoutAndroid</Title>
+      </ScrollView>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 15,
-        backgroundColor: '#fff',
-    },
-
-    subContainer: {
-
-    }
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff'
+  },
+  subContainer: {},
+  text: {
+    color: 'red',
+    marginTop: 10
+  },
+  title: {
+    fontSize: 24,
+    marginTop: 10
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
